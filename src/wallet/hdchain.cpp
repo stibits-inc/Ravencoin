7630fbb80e1334792bbd0dd7633ab2d5760d910c
omari <omari@stibits.io>
2019-05-24 11:24:59 +0000

    Step 1

---
+++ src/wallet/hdchain.cpp
@@ -0,0 +1,4 @@
+//
+// Created by omari on 2019-05-23.
+//
+
---
+++ src/wallet/hdchain.h
@@ -0,0 +1,8 @@
+//
+// Created by omari on 2019-05-23.
+//
+
+#ifndef RAVEN_HDCHAIN_H
+#define RAVEN_HDCHAIN_H
+
+#endif //RAVEN_HDCHAIN_H
